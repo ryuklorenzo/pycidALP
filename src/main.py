617e693b2ec06648d34aa39bf9dcb74ecdba00cf
ryuk_Lorenzo <angel14lpl@gmail.com>
2026-01-12 20:34:55 +0100

    First commit

---
+++ src/main.py
@@ -0,0 +1,6 @@
+def add(n1: int, n2: int) -> int:
+    return n1 + n2
+
+
+if __name__ == "__main__":
+    add(10, 5)
---
+++ tests/test_add.py
@@ -0,0 +1,5 @@
+from src.main import add
+
+
+def test_add1():
+    assert add(10, 10) == 20
